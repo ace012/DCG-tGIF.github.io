cc0bdcbfce0485d4d1ed020925778002f80e7993
ace9934 <ace9934@kaist.ac.kr>
2021-03-31 08:16:34 +0900

    Update new character 210325

--- static/scripts/validCheck.py
+++ static/scripts/validCheck.py
@@ -33,31 +33,33 @@ def checkChild(mN):
         width, height = img.size
         if not math.log(width, 2).is_integer() or not math.log(height, 2).is_integer():
             print mN + ": invalid texture image - texture_01.png"
+    
+    jsonFile = path + "MOC." + mN + ".json"
+    if os.path.isfile(jsonFile):
+        with open(path + "MOC." + mN + ".json") as json_file:
+            data = json.load(json_file)
 
-    with open(path + "MOC." + mN + ".json") as json_file:
-        data = json.load(json_file)
+            for t in data["textures"]:
+                if t != "texture_00.png" and t != "texture_01.png":
+                    print mN + ": weird texture file name - " + t
 
-        for t in data["textures"]:
-            if t != "texture_00.png" and t != "texture_01.png":
-                print mN + ": weird texture file name - " + t
+            if data["model"] != "character.dat":
+                print mN + ": weird model file name - " + data["model"]
 
-        if data["model"] != "character.dat":
-            print mN + ": weird model file name - " + data["model"]
+            for m in data["motions"]:
+                if data["motions"][m][0]["file"] != mN + "_" + m + ".mtn":
+                    print mN + ": weird motion file name - " + data["motions"][m][0]["file"]
 
-        for m in data["motions"]:
-            if data["motions"][m][0]["file"] != mN + "_" + m + ".mtn":
-                print mN + ": weird motion file name - " + data["motions"][m][0]["file"]
-
-        if "expressions" in data:
+            if "expressions" in data:
 #            if mN[0] == 's' or mN[0] == 'm':
 #                print mN + " has expression file!"
 #            if mN[0] == 'm':
 #                print mN + " has exprs!"
-            for expr in data["expressions"]:
-                if not os.path.isfile(path + expr["file"]):
-                    print mN + ": expression file does not exist - " + expr["file"]
+                for expr in data["expressions"]:
+                    if not os.path.isfile(path + expr["file"]):
+                        print mN + ": expression file does not exist - " + expr["file"]
 
-            exprList.append(mN)
+                exprList.append(mN)
 
 #        elif mN[0] != 's' and mN[0] != 'm':
 #            print mN + ": doesn't have expression file!"
